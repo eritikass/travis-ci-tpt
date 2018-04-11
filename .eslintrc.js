178de8f3662b0d23a35a2dea840310e382f5ca20
Siim Tiilen <eritikass@gmail.com>
2018-04-11 08:49:54 +0300

    win fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,5 +47,12 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
 	'linebreak-style': 0,
+	
+	'import/extensions': 0,
+	'import/no-unresolved': 0,
+	'import/no-extraneous-dependencies': 0,
+	'import/no-named-as-default-member': 0,
+	'import/no-named-as-default': 0,
+	'import/no-duplicates': 0,
   }
 }
