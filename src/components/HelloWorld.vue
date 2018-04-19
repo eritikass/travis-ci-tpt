6193df0c6ea79939a46a7afc1a31967f35c64762
Siim Tiilen <eritikass@gmail.com>
2018-04-19 17:52:59 +0300

    update testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to siim homepage!',
+      msg: 'Welcome to someone homepage!',
     };
   },
 };
---
+++ test/unit/specs/sum.spec.js
@@ -0,0 +1,18 @@
+function sum(a, b = 0) {
+  return parseInt(a, 10) + parseInt(b, 10);
+}
+
+describe('sum', () => {
+  it('3+5 = 8', () => {
+    expect(sum(3, 5)).toEqual(8);
+  });
+  it('"0"+"0" = 0', () => {
+    expect(sum('0', '0')).toEqual(0);
+  });
+  it('"2"+3 = 5', () => {
+    expect(sum('2', 3)).toEqual(5);
+  });
+  it('3 = 8', () => {
+    expect(sum(3)).toEqual(3);
+  });
+});
