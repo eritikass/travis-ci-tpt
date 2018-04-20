c2341c749e0d5e74344ce935ec5c2ca10c86bc4f
Siim Tiilen <eritikass@gmail.com>
2018-04-20 09:30:53 +0300

    test case 1

---
+++ test/unit/specs/text.spec.js
@@ -0,0 +1,12 @@
+function text(t = '') {
+  return t;
+}
+
+// test case1: returns same text as given in param
+// test case2: if no text given, returns empty string
+describe('text', () => {
+    it ('returns same text as given in param', () => {
+        const t1 = text('kala');
+        expect(t1).toBe('kala');
+    });
+});
--- test/unit/specs/text.sum.js
+++
@@ -1,6 +0,0 @@
-function text(t = '') {
-  return t;
-}
-
-// test case1: returns same text as given in param
-// test case2: if no text given, returns empty string
