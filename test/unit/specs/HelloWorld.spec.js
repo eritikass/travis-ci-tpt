db895a5b91901f182e97ad024aa4f2bba1351d0f
Siim Tiilen <eritikass@gmail.com>
2018-04-20 08:53:54 +0300

    test3

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -8,4 +8,10 @@ describe('HelloWorld.vue', () => {
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to someone homepage!');
   });
+
+  it('we have 2 buttons in div.buttons', ()   => {
+    const Constructor = Vue.extend(HelloWorld);
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelectorAll('div.buttons button').length).toBe(2);
+  });
 });
