5b347286e22736306fb2d7283bca1bb2d37b5429
Siim Tiilen <eritikass@gmail.com>
2018-04-25 08:57:44 +0300

    group testing

--- test/unit/specs/sum.spec.js
+++ test/unit/specs/sum.spec.js
@@ -6,13 +6,15 @@ describe('sum', () => {
   it('3+5 = 8', () => {
     expect(sum(3, 5)).toEqual(8);
   });
-  it('"0"+"0" = 0', () => {
-    expect(sum('0', '0')).toEqual(0);
-  });
-  it('"2"+3 = 5', () => {
-    expect(sum('2', 3)).toEqual(5);
-  });
   it('3 = 8', () => {
     expect(sum(3)).toEqual(3);
+  });
+  describe('with strings', () => {
+    it('"0"+"0" = 0', () => {
+      expect(sum('0', '0')).toEqual(0);
+    });
+    it('"2"+3 = 5', () => {
+      expect(sum('2', 3)).toEqual(5);
+    });
   });
 });
