232403b1f45f9a00ade7617734808b7ea704f2ae
Siim Tiilen <eritikass@gmail.com>
2018-04-26 17:49:31 +0300

    use yarn in ci

---
+++ test/unit/specs/incrementByRandom.spec.js
@@ -0,0 +1,22 @@
+import random from '../../../src/random';
+
+jest.mock('../../../src/random');
+
+random.mockImplementation(() => 100);
+
+function incrementByRandom(input) {
+  return input + random();
+}
+
+describe('incrementByRandom', () => {
+  it('5 + random => 105', () => {
+    const number = incrementByRandom(5);
+    expect(number).toBe(105);
+  });
+
+  it('13 + random => 113', () => {
+    const number = incrementByRandom(13);
+    expect(number).toBe(113);
+  });
+});
+
